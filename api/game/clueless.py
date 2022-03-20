265895b1682049f708013291127199268df9e975
R2DLiu <zliu3301@gmail.com>
2022-03-20 17:51:44 -0400

    restructure components

--- api/game/clueless.py
+++ api/game/clueless.py
@@ -1,27 +1,32 @@
-
 class Clueless:
     # TODO add game logic
 
     # TODO give connection manager to clueless class
     def __init__(self, *args, **kwargs):
-        self.rooms = ['study', 'hall', 'lounge', 'library', 'billiard',
-                      'dining', 'conservatory', 'ballroom', 'kitchen']
-        self.suspects = ['colonel_mustard', 'miss_scarlet', 'professor_plum',
-                         'mr_green', 'mrs_white', 'mrs_peacock']
-        self.weapons = ['rope', 'lead_pipe', 'knife', 'wrench', 
-                        'candlestick', 'revolver']
-        self.cards  = self.suspects + self.weapons + self.rooms
-        
-        self.hallways = ['study_hall', 'hall_lounge', 'lounge_dining',
-                         'dining_kitchen', 'kitchen_ballroom', 
-                         'ballroom_conservatory', 'conservatory_library',
-                         'library_study', 'hall_billiard', 'dining_billiard',
-                         'ballroom_billiard', 'library_billiard']
+        self.rooms = [
+            'study', 'hall', 'lounge', 'library', 'billiard', 'dining',
+            'conservatory', 'ballroom', 'kitchen'
+        ]
+        self.suspects = [
+            'colonel_mustard', 'miss_scarlet', 'professor_plum', 'mr_green',
+            'mrs_white', 'mrs_peacock'
+        ]
+        self.weapons = [
+            'rope', 'lead_pipe', 'knife', 'wrench', 'candlestick', 'revolver'
+        ]
+        self.cards = self.suspects + self.weapons + self.rooms
+
+        self.hallways = [
+            'study_hall', 'hall_lounge', 'lounge_dining', 'dining_kitchen',
+            'kitchen_ballroom', 'ballroom_conservatory',
+            'conservatory_library', 'library_study', 'hall_billiard',
+            'dining_billiard', 'ballroom_billiard', 'library_billiard'
+        ]
         self.secret_passages = ['study_kitchen', 'lounge_conservatory']
 
         # players dict holds client_id: character assignments
         self.players = {}
-        self.state = initialize_board(players)
+        self.state = self.initialize_board(self.players)
 
     def initialize_board(self, players):
         """ Places characters in starting locations, generates sceanario, 
@@ -29,17 +34,17 @@ class Clueless:
 
         # data structs are just placeholders; can/should be changed
         state = {
-            'suspect_locations': {}, # dict of suspect: room/hallway loc
-            'concealed_scenario': {}, 
-            'player_cards': {}, # dict of player: player's card list 
-            'visible_cards': [], # list of cards shown to all players
-            'turn_order': [], # player turn order
-            'current_turn': 0, # whose turn is it
-            'suggestion': {} # holds current suggestion players must disprove
+            'suspect_locations': {},  # dict of suspect: room/hallway loc
+            'concealed_scenario': {},
+            'player_cards': {},  # dict of player: player's card list 
+            'visible_cards': [],  # list of cards shown to all players
+            'turn_order': [],  # player turn order
+            'current_turn': 0,  # whose turn is it
+            'suggestion': {}  # holds current suggestion players must disprove
         }
         # starting locations
         state['suspect_locations'] = {
-            'colonel_mustard':' lounge_dining',
+            'colonel_mustard': ' lounge_dining',
             'miss_scarlet': 'hall_lounge',
             'professor_plum': 'library_study',
             'mr_green': 'ballroom_conservatory',
@@ -51,25 +56,24 @@ class Clueless:
 
     def create_scenario(self):
         # picks a random weapon, suspect, room to store in case envelope
-        pass 
-    
+        pass
+
     def distribute_cards(self):
         # randomly distribute cards
-        pass 
-    
+        pass
+
     def generate_turn_order(self):
         # randomly picks player move order
-        pass 
-    
+        pass
+
     def get_next_player(self, current_player):
         # returns next player to move
-        pass 
-    
+        pass
+
     def allowed_moves(self, player):
         # returns list of allowed moves
-        pass 
-    
-    def verify_accusation(self, accusation):
-        # returns 
         pass
-    
+
+    def verify_accusation(self, accusation):
+        # returns
+        pass
