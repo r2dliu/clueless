37ace82f9396d65bcdfa6224f37dafe98b86844e
jsquires0 <jws422@cornell.edu>
2022-04-28 19:07:23 -0400

    moved buttons to sidebar, moved formatLabel to utils

--- web/components/Canvas/Sidebar/Sidebar.jsx
+++ web/components/Canvas/Sidebar/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
 import { GameContext } from "@/components/helpers/GameContext";
 import Chat from "./Chat";
+import Controls from "./Controls";
 import styles from "./Sidebar.module.scss";
+
 
 function Sidebar() {
   return (
     <div className={styles.Sidebar}>
       <div>
         <Chat />
+        <Controls />
       </div>
     </div>
   );
