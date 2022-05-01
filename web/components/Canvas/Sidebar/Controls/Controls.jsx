30056c920ff181eaa4b7708de469fc79ce8cd8ba
Rachel <rachelnewkirk7@gmail.com>
2022-05-01 13:43:05 -0400

    Hide movement buttons after being clicked

--- web/components/Canvas/Sidebar/Controls/Controls.jsx
+++ web/components/Canvas/Sidebar/Controls/Controls.jsx
@@ -154,6 +154,7 @@ function Controls() {
                                         location: move,
                                     })
                                 );
+                                setAction("");
                             }}
                             disabled={isControlsLocked}
                         >
