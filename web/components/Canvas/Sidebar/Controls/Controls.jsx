f5c6ada6f159f8661001fa02423091a43f94a17d
rachelnewkirk <rachelnewkirk7@gmail.com>
2022-05-01 19:39:52 -0400

    28 make player action options disappear when clicked or cancelled (#29)
    * Move options disappear when clicked; secret passage bug fix
    
    * Change 'cancel' to 'clear' for suggestions and accusations to better reflect actual functionality

--- web/components/Canvas/Sidebar/Controls/Controls.jsx
+++ web/components/Canvas/Sidebar/Controls/Controls.jsx
@@ -154,6 +154,7 @@ function Controls() {
                                         location: move,
                                     })
                                 );
+                                setAction("");
                             }}
                             disabled={isControlsLocked}
                         >
@@ -165,16 +166,16 @@ function Controls() {
 
             {gameState.game_phase === 1 && action == "end_turn" && (
                 <Button
-                key="end_turn"
-                variant="outlined"
-                onClick={() => {
-                    websocket?.current?.send(
-                    JSON.stringify({
-                        type: "end_turn",
-                    })
-                    );
-                }}
-                disabled={isControlsLocked}
+                    key="end_turn"
+                    variant="outlined"
+                    onClick={() => {
+                        websocket?.current?.send(
+                            JSON.stringify({
+                                type: "end_turn",
+                            })
+                        );
+                    }}
+                    disabled={isControlsLocked}
                 >
                     Submit
                 </Button>
@@ -276,7 +277,7 @@ function Controls() {
                             }}
                             disabled={isControlsLocked}
                         >
-                            Cancel Suggestion
+                            Clear Suggestion
                         </Button>
                     </div>
                     <div className={styles.submission}></div>
@@ -382,7 +383,7 @@ function Controls() {
                             }}
                             disabled={isControlsLocked}
                         >
-                            Cancel Accusation
+                            Clear Accusation
                         </Button>
                     </div>
                 </div>
