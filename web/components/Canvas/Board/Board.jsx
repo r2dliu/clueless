34c8bf17a43f3ede18283e1e2c8f8eba648a686d
Ben Ferro <ben.p.ferro@gmail.com>
2022-05-03 22:42:04 -0500

    added movement rules

--- web/components/Canvas/Board/Board.jsx
+++ web/components/Canvas/Board/Board.jsx
@@ -92,12 +92,6 @@ function Board() {
 
   }, [gameState]);
 
-
-  useEffect(() => {
-    console.log('rules open -> ' + rulesOpen)
-
-  }, [rulesOpen]);
-
   return (
     <div className={styles.Board}>
       {/* message dump */}
@@ -170,28 +164,30 @@ function Board() {
             </DialogTitle>
             <DialogContent dividers>
               <DialogContentText>
-                <Typography gutterBottom paragraph>
+                <Typography gutterBottom paragraph className={styles.header}>
                   Mr. Boddy has been murdered in his mansion. You must find out who killed him, what did they kill him with, and where the murder happened.
                 </Typography>
 
-                <Typography>
+                <Typography className={styles.header}>
                   Game Play
                 </Typography>
                 <Typography gutterBottom paragraph>
-                  On your turn, you may move to any room, hallway, or secret passage adjacent to your current location.
+                  On your turn, you may move to any room, hallway, or secret passage adjacent to your current location. Only one player may be in a hallway at a time.
+                  If another player is in a hallway, you cannot move through it. If you begin your turn in a hallway, you must move to one of the adjacent rooms and make
+                  a suggestion.
                 </Typography>
 
-                <Typography>
+                <Typography className={styles.header}>
                   Suggestions
                 </Typography>
                 <Typography gutterBottom paragraph>
                   You may make a suggestion anytime you enter a room. You must provide a suspect, a murder weapon, and the room you are in. For example, Mrs. White
-                  in the library with the rope. Then your opponents (going in the turn order) must prove your suggestion false by showing you one of their cards 
-                  that matches your suggestion. The suggestion ends when either a player has disproved your suggestion, or all players have passed. After making a
-                  suggestion you may make an accusation or end your turn.
+                  in the library with the rope. Then going in the turn order, your opponents must prove your suggestion false by showing you one of their cards 
+                  that matches your suggestion (or pass if they cannot disprove the suggestion). The suggestion ends when either a player has disproved your suggestion,
+                  or all players have passed. After making a suggestion you may make an accusation or end your turn.
                 </Typography>
 
-                <Typography>
+                <Typography className={styles.header}>
                   Accusations
                 </Typography>
                 <Typography gutterBottom paragraph>
@@ -202,7 +198,10 @@ function Board() {
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button onClick={closeRulesDialog}>Let&apos;s get started</Button>
+              <Button onClick={closeRulesDialog}
+                      variant="contained">
+                        Let&apos;s get started
+              </Button>
             </DialogActions>
           </Dialog>
         </div>
